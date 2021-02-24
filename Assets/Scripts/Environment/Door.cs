f60555f9522fffdca835363bfdb7b7394f8905b4
HellcatDev <jakobsimson@gmail.com>
2021-02-24 15:22:52 +0800

    stuff

---
+++ Assets/Scripts/Environment/Door.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : InteractableObject
+{
+    private Animator anim;
+    private bool open = false;
+
+    private void Awake()
+    {
+        anim = gameObject.transform.GetComponent<Animator>();
+    }
+
+    public override void Activate()
+    {
+        if (open)
+            {
+            anim.SetBool("Opened", false);
+            open = false;
+        }
+        else
+        {
+            anim.SetBool("Opened", true);
+            open = true;
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    public float interactionDistance;
+    public float interactionDistance = 2.5f;
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, interactionDistance)) {
+                Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * interactionDistance, Color.green, 0.5f);
+                InteractableObject obj = hit.transform.GetComponent<InteractableObject>();
+                if (obj != null)
+                {
+                    obj.Activate();
+                }
+            }
+        }
     }
 }
