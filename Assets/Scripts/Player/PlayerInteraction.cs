c2e2d211c88428f06109794fbddf3a7a218237b6
HellcatDev <jakobsimson@gmail.com>
2021-03-24 11:05:28 +0800

    Update PlayerInteraction.cs
    Converted back to old version for submission

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -12,11 +12,11 @@ public class PlayerInteraction : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out RaycastHit hit, interactionDistance)) {
-                Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * interactionDistance, Color.green, 0.5f);
                 InteractableObject obj = hit.transform.GetComponent<InteractableObject>();
                 if (obj != null)
                 {
                     obj.Activate();
+                    Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * interactionDistance, Color.green, 0.5f);
                 }
             }
         }
